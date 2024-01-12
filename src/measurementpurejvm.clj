c1d372f15a06303db663e58ce4420473d70424a4
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-01-12 08:15:07 -0300

    Performance improvement with juxt function

--- src/measurementpurejvm.clj
+++ src/measurementpurejvm.clj
@@ -1,8 +1,7 @@
 (ns measurementpurejvm
   (:require [clojure.data.csv :as csv]
             [babashka.fs :as fs]
-            [clojure.java.io :as io]
-            [clojure.pprint :as p]
+            [clojure.java.io :as io] 
             [clojure.string :as string]
             [criterium.core :refer [bench]])
   (:import java.io.IOException)
@@ -29,7 +28,7 @@
 (defn extraer-datos [s]
   (nthrest s 2))
 
-(defn parsear [s]
+(defn parsear [s] 
   (pmap
    (comp
     (fn [arr]
@@ -38,18 +37,16 @@
    s))
 
 (defn ->map [s]
-  (->> (doall (pmap #(apply hash-map %) s))
+  (->> (pmap #(apply hash-map %) s)
        (apply merge-with vector)))
 
-(defn aplicar-calculos [v] 
+(defn aplicar-calculos [v]
+  #_(println v)
   (let [len (count v)
-        mean (-> (reduce + v)
-                 (/ len))
-        -min (apply min v)
-        -max (apply max v)]
+        [-min mean -max] ((juxt #(apply min %) #(-> (reduce + %) (/ len)) #(apply max %)) v)]
     (format "%1.1f/%2.1f/%3.1f" -min mean -max)))
 
-
+ 
 (defn formatear [coll]
   (doseq [[k v] coll :let [temps (-> (if (vector? v) 
                                  (flatten v)
@@ -65,24 +62,24 @@
       formatear))
 
 (defn -main [& args]
-  #_(bench) 
-  (let [arg args]
-    (try
-      (when-let [p (valido? arg)] 
-        (time (run p)))
-      (catch IOException e (prn (.getMessage e)))
-      (catch Exception e (prn (.getMessage e)))))) 
+  (bench 
+   (let [arg args]
+     (try
+       (when-let [p (valido? arg)] 
+         (time (run p)))
+       (catch IOException e (prn (.getMessage e)))
+       (catch Exception e (prn (.getMessage e))))))) 
  
 (comment 
   (leer "Doc24 - Planilla Copagos COMPLETA.csv")
   (let [x (csv/read-csv (io/reader "C:\\Users\\jrivero\\Downloads\\weather_stations.csv"))]
-    (parsear (nthrest x 2)))  
-  
+    (nthrest (nthrest x 2) 20))  
+   
   (let [m (->> (leer "C:\\Users\\jrivero\\Downloads\\weather_stations.csv") 
               extraer-datos
               parsear 
-              #_->map
-               #_formatear
+              ->map
+               formatear
               #_(pmap #(apply hash-map %))
               #_frequencies 
               #_(sort-by val >)
@@ -90,19 +87,29 @@
                #_(filter (or char? string?)))]
     #_(flatten (get m "Dumri"))
     m
-    #_(formatear m))
-
+    #_(formatear m)) 
+ 
   (format "%1.1f/%2.1f/%3.1f" 10.3434 20.35454 20.5443)
+ 
+  (seq (lazy-seq [2 4]))
+  (reduce conj 31.3 [33.63])
   
   ((fn [[a b]] [a (Double/parseDouble b)]) ["Tojio" "20.20"])
 
   (apply hash-map ["a" 23])
-  (merge-with (fn [& arg] (apply vector arg)) {"a" 10.4} {"b" 333} {"a" 19.4} {"c" 43} {"b" 4})
+  (merge-with (fn [elem1 elem2]
+                (let [col (if (coll? elem2) elem2 (list elem2))]
+                  (cons elem1 (lazy-seq col)))) 
+              {"a" 10.4} {"b" 333} {"a" 19.4} {"c" 43} {"b" 4})
 
-  (aplicar-calculos [20.3232 32.92332 12.9012 32.983223 78.34232323 89.3443234 56.9993443 14.423433214])
-  
+  (time (aplicar-calculos [20.3232 32.92332 12.9012 32.983223 78.34232323 89.3443234 56.9993443 14.423433214])) 
+  (time ((juxt #(-> (reduce + %) (/ (count %))) #(apply min %) #(apply max %)) [20.3232 32.92332 12.9012 32.983223 78.34232323 89.3443234 56.9993443 14.423433214]))
   (filter (or (complement number?) coll?) '(3 53 3 'a "fd" true {:a 3} [4 53]))
   (filter (or (complement number?) coll?) '(3 53 3 34 54 544 64 '(((32) 443) 4343)))
+  
+(- 240 164)
+
+  ;; Quité el doall en el pmap y obtuvo una mejora leve en el tiempo
   )
 
  
